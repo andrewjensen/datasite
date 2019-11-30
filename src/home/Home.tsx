1951b7605343ccffd00b617bcd29fe00e9a6f13d
Andrew Jensen <andrewjensen90@gmail.com>
2019-11-29 17:44:16 -0700

    Add styles to Dashboard list

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,8 +1,12 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
 import Markdown from 'markdown-to-jsx';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 import LayoutContainer from '../common/components/LayoutContainer';
 import ManifestContext from '../common/state/ManifestContext';
@@ -58,19 +62,40 @@ interface DashboardListProps {
   dashboards: ManifestDashboard[]
 }
 
-const DashboardList: React.FC<DashboardListProps> = ({ dashboards }) => (
-  <DashboardListContainer>
-    <Typography variant="h4">Dashboards</Typography>
-    {dashboards.map(dashboard => (
-      <p>
-        <Link to={`/dashboard/${dashboard.slug}`}>
-          {dashboard.title}
-        </Link>
-      </p>
-    ))}
-  </DashboardListContainer>
-);
+const DashboardList: React.FC<DashboardListProps> = ({ dashboards }) => {
+  const history = useHistory();
+
+  const navigateTo = (location: string) => {
+    history.push(location);
+  };
+
+  return (
+    <DashboardListContainer>
+      <DashboardListTitle>
+        <Typography variant="h4">Dashboards</Typography>
+      </DashboardListTitle>
+      <Paper>
+        <List component="nav">
+          {dashboards.map(dashboard => (
+            <ListItem
+              key={dashboard.slug}
+              button
+              onClick={() => navigateTo(`/dashboard/${dashboard.slug}`)}
+            >
+              <ListItemText primary={dashboard.title} />
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+    </DashboardListContainer>
+  );
+};
 
 const DashboardListContainer = styled.div`
 
+`;
+
+const DashboardListTitle = styled.div`
+  margin-top: 2rem;
+  margin-bottom: 1rem;
 `;
